aeeb42ff7371b9184fa14059511e8aaeef16d0ae
Arthur Dexter <arthur.dexter@rethinkbooks.com>
2011-07-26 10:00:00 -0500

    Import UIKit.h.

--- EGOImageLoader/EGOImageLoader.h
+++ EGOImageLoader/EGOImageLoader.h
@@ -24,7 +24,7 @@
 //  THE SOFTWARE.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @protocol EGOImageLoaderObserver;
 @interface EGOImageLoader : NSObject/*<NSURLConnectionDelegate>*/ {
